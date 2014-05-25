00057b73f6a2040da07d21bbd37767ff65599e0b
Anna Pilipenko <pilipenko7464@gmail.com>
2014-05-24 19:38:54 -0700

    Last changes

--- src/main/java/core/QA.java
+++ src/main/java/core/QA.java
@@ -1,5 +1,32 @@
 package core;
 
-public class QA {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class QA {
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+//		WebDriver driver = new HtmlUnitDriver();
+		
+		String url = "http://www.learn2test.net";
+		String title_expected = "learn2test.net";
+		
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		String title_actual = driver.getTitle();
+		if (title_expected.equals(title_actual)) {
+			System.out.println("PASSED");
+		} else 		
+			{
+		  			System.out.println("FAILED");			
+
+			}
+		driver.quit();
+	}
 }
+
+	
+
